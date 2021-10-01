d7e5aa6a9fefd7f815debfc2722de6a66d526e89
Makarius Salib <misalib@cpp.edu>
2021-10-01 15:00:15 -0700

    Adds searching by phone and test cases

--- Class Work/Assignment #2/employeeMain.c
+++ Class Work/Assignment #2/employeeMain.c
@@ -8,6 +8,7 @@ int main(void)
 {
    PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
    PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind);
+   PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char * targetPhone);
 
    extern Employee EmployeeTable[];
    extern const int EmployeeTableEntries;
@@ -25,6 +26,21 @@ int main(void)
       printf("Employee Tony Bobcat is in record %d\n", matchPtr - EmployeeTable);
    else
       printf("Employee Tony Bobcat is NOT found in the record\n");
+
+
+   //No match with phone
+   matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "714-555-2759");
+   if (matchPtr != NULL)
+      printf("Employee with phone number %s is in record %d\n", "714-555-2759", matchPtr - EmployeeTable);
+   else
+      printf("Employee phone number is NOT found in the record\n");
+
+   //Match with phone number
+   matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "714-555-2749");
+   if (matchPtr != NULL)
+      printf("Employee with phone number %s is in record %d\n", "714-555-2749", matchPtr - EmployeeTable);
+   else
+      printf("Employee phone number is NOT found in the record\n");
 
    return EXIT_SUCCESS;
 
